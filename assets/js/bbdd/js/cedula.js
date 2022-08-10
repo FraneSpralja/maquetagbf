f0ab011855cc4cd7562452bf0c28f8152ee9dd0f
FraneSpralja <spralja.frane@gmail.com>
2022-08-10 00:49:07 -0400

    definir estilos básicos lista cedulas

--- assets/js/bbdd/js/cedula.js
+++ assets/js/bbdd/js/cedula.js
@@ -20,14 +20,20 @@ function  getImagenFrontURL() {
     .then((snapshot) => {
         const listGroup = document.createElement('ul');
         listGroup.classList.add('lista-cedula');
+
+        const title = document.createElement('h3');
+        title.textContent = 'Imagen Delantera'
+
         snapshot.forEach((dataImg) => {
                 // console.log(dataImg.val())
                 const { ImageName, ImageURL } = dataImg.val();
+
+                const listItem = document.createElement('li')
                 
-                const itemName = document.createElement('li')
+                const itemName = document.createElement('span')
                 itemName.textContent = ImageName;
 
-                const itemURL = document.createElement('li')
+                const itemURL = document.createElement('span')
                 const linkURL = document.createElement('a');
                 linkURL.textContent = 'front';
                 linkURL.href = ImageURL;
@@ -36,7 +42,7 @@ function  getImagenFrontURL() {
                 
                 itemURL.appendChild(linkURL);
 
-                const itemImagen = document.createElement('li');
+                const itemImagen = document.createElement('span');
                 const image = document.createElement('img');
                 image.src = linkURL;
                 image.setAttribute('width', "150");
@@ -44,10 +50,13 @@ function  getImagenFrontURL() {
 
                 itemImagen.appendChild(image);
 
-                listGroup.appendChild(itemName);
-                listGroup.appendChild(itemURL);
-                listGroup.appendChild(itemImagen);
+                listItem.appendChild(itemName);
+                listItem.appendChild(itemURL);
+                listItem.appendChild(itemImagen);
+
+                listGroup.appendChild(listItem);
             })
+            frontCedula.appendChild(title);
             frontCedula.appendChild(listGroup);
     })
 }
@@ -57,23 +66,30 @@ function getImagenBackURL(){
     get(child(dbRef, "imagesBack"))
     .then((snapshot) => {
         const listGroup = document.createElement('ul');
+        listGroup.classList.add('lista-cedula');
+
+        const title = document.createElement('h3');
+        title.textContent = 'Imagen Delantera'
+
         snapshot.forEach((dataImg) => {
                 // console.log(dataImg.val())
                 const { ImageName, ImageURL } = dataImg.val();
+
+                const listItem = document.createElement('li')
                 
-                const itemName = document.createElement('li')
+                const itemName = document.createElement('span')
                 itemName.textContent = ImageName;
 
-                const itemURL = document.createElement('li')
+                const itemURL = document.createElement('span')
                 const linkURL = document.createElement('a');
-                linkURL.textContent = 'front';
+                linkURL.textContent = 'back';
                 linkURL.href = ImageURL;
                 linkURL.target = '_blank';
                 linkURL.setAttribute('download', ImageName);
                 
                 itemURL.appendChild(linkURL);
 
-                const itemImagen = document.createElement('li');
+                const itemImagen = document.createElement('span');
                 const image = document.createElement('img');
                 image.src = linkURL;
                 image.setAttribute('width', "150");
@@ -81,11 +97,13 @@ function getImagenBackURL(){
 
                 itemImagen.appendChild(image);
 
+                listItem.appendChild(itemName);
+                listItem.appendChild(itemURL);
+                listItem.appendChild(itemImagen);
 
-                listGroup.appendChild(itemName);
-                listGroup.appendChild(itemURL);
-                listGroup.appendChild(itemImagen);
+                listGroup.appendChild(listItem);
             })
+            backCedula.appendChild(title);
             backCedula.appendChild(listGroup);
     })
 }
